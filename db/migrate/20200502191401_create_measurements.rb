2a4afa80d9f258001f4fa99d6c0c8f152c26a6f8
secondbaker <secondbaker@gmail.com>
2020-05-02 15:24:39 -0400

    Database migrated and ready to go.

--- db/migrate/20200502191401_create_measurements.rb
+++ db/migrate/20200502191401_create_measurements.rb
@@ -2,7 +2,7 @@ class CreateMeasurements < ActiveRecord::Migration[6.0]
   def change
     create_table :measurements do |t|
       t.numeric :value
-      t.DateTime :date_time
+      t.datetime :date_time
       t.text :notes
 
       t.timestamps
