1985fbae965113576667e4a398feeb04754e02ba
secondbaker <secondbaker@gmail.com>
2020-05-02 16:25:58 -0400

    Actually, that change should happen in the model, of course.

--- app/models/measurement_datum.rb
+++ app/models/measurement_datum.rb
@@ -1,2 +1,9 @@
 class MeasurementDatum < ApplicationRecord
+    before_save :set_initial_graph_time
+
+    def set_initial_graph_time
+        unless self.graph_time
+            self.graph_time = self.created_at
+        end
+    end
 end
