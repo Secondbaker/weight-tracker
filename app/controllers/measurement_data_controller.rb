7ea333c977fe614878c3a23827c54881516f919f
secondbaker <secondbaker@gmail.com>
2020-05-07 14:46:55 -0400

    Something to do later:  Put the chart into a partial to render wherever it shows up.
    For now, it's time to try implementing a CSS framework.

--- app/controllers/measurement_data_controller.rb
+++ app/controllers/measurement_data_controller.rb
@@ -46,7 +46,7 @@ class MeasurementDataController < ApplicationController
   def update
     respond_to do |format|
       if @measurement_datum.update(measurement_datum_params)
-        format.html { redirect_to @measurement_datum, notice: 'Measurement datum was successfully updated.' }
+        format.html { redirect_to @measurement_datum.data_group, notice: 'Measurement datum was successfully updated.' }
         format.json { render :show, status: :ok, location: @measurement_datum }
       else
         format.html { render :edit }
