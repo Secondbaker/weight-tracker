f931fe06281dd55a7cda2d03268cb4377c437fda
Austin Baker <secondbaker@Austins-MacBook.local>
2020-05-11 22:12:39 -0400

    Gave the graphs a default range that more closely fits the data.

--- app/models/data_group.rb
+++ app/models/data_group.rb
@@ -2,4 +2,17 @@ class DataGroup < ApplicationRecord
     validates_presence_of :name
     has_many :measurement_data, dependent: :destroy
     belongs_to :user
+
+    def lowest
+        self.measurement_data.min
+    end
+    def highest
+        self.measurement_data.max
+    end
+    def graph_min
+        (self.lowest.value - (self.highest.value - self.lowest.value) * 0.05).floor
+    end
+    def graph_max
+        (self.highest.value + (self.highest.value - self.lowest.value) * 0.05).ceil
+    end
 end
